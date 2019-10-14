e0ec80f5c89afbfe05a7d80e7f9cbb03a6a5f772
nhoxuan <nhoxuan@atlassian.com>
2019-10-15 09:32:40 +1100

    ts-loader + fork-ts-checker-webpack-plugin: when a change is made in a source file that will not emit js it will not trigger the type checker whilst in watch mode

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,19 @@
-import React, { SFC } from 'react';
+import React, { PureComponent, SFC } from 'react';
+import { HelloProps } from './types';
+// export interface HelloProps {
+//   name: string;
+//   age:number;
+// }
 
-export interface HelloProps {
-  name: string;
-}
-
-export const Hello: SFC<HelloProps> = ({ name }) => (
-  <h1>{ name }</h1>
+export const Hello2: SFC<HelloProps> = ({ name }) => (
+    <h1>{name}</h1>
 );
+
+export class Hello extends PureComponent<HelloProps>{
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        const {name} = this.props;
+        return (
+            <h1>{name}</h1>
+        )
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,60 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+var ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const APP_PATH = path.resolve(__dirname, 'src');
 
 module.exports = {
-  entry: APP_PATH,
+    entry: APP_PATH,
 
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.json',
+        ],
+    },
 
-  module: {
-    rules: [{ test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }],
-  },
+    module: {
+        rules: [
+            // { test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    // disable type checker - we will use it in fork plugin
+                    transpileOnly: true,
+                },
+            },
+        ],
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({ inject: true, template: path.join(APP_PATH, 'index.html') }),
-    new ForkTsCheckerWebpackPlugin(),
-  ]
+    devServer: {
+        overlay: {
+            warnings: true,
+            errors: true,
+        },
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.join(APP_PATH, 'index.html'),
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+            checkSyntacticErrors: true,
+        }),
+        new ForkTsCheckerNotifierWebpackPlugin({
+            title: 'TypeScript',
+            alwaysNotify: true,
+        }),
+    ],
 };
